8d7ced45a790cfdf86e8085460a6d654777ecfcf
PaulNaval <22-1-00759@vsu.edu.ph>
2025-03-10 18:46:11 +0800

    first commit

--- components/Content/DesktopContent.tsx
+++ components/Content/DesktopContent.tsx
@@ -2,14 +2,11 @@
 import React from "react";
 import { useGlobalContext } from "@/Context/GlobalProvider";
 import DesktopDashboard from "./Dashboard/DesktopDashboard";
-import DesktopMap from "./Map/DesktopMap";
-import { getBgImage } from "@/Utils/getBgImage";
 const DesktopContent = () => {
   const { menu } = useGlobalContext();
   return (
     <div className="h-full flex flex-shrink object-cover">
       {menu === "Dashboard" && <DesktopDashboard />}
-      {menu === "Map" && <DesktopMap />}
     </div>
   );
 };
